4b4bdc0a4c22e397b8bca5e9fcc7fddb49131f92
João Pedro <joaopdesousa12@gmail.com>
2025-02-21 23:46:07 -0300

    fix(User): fix incorrect table name from table user

--- src/main/java/com/GerenciadorEstoque/GerenEstoque/Models/User.java
+++ src/main/java/com/GerenciadorEstoque/GerenEstoque/Models/User.java
@@ -1,9 +1,6 @@
 package com.GerenciadorEstoque.GerenEstoque.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -11,6 +8,7 @@ import java.io.Serializable;
 import java.util.Collection;
 
 @Entity
+@Table(name = "tb_users")
 public class User implements Serializable, UserDetails {
 
     @Id
@@ -21,6 +19,50 @@ public class User implements Serializable, UserDetails {
     private String address;
     private String corporateEmail;
     private String password;
+
+    public User() {
+    }
+
+    public User(String id, String username, String CNPJ, String address, String corporateEmail, String password) {
+        this.id = id;
+        this.username = username;
+        this.CNPJ = CNPJ;
+        this.address = address;
+        this.corporateEmail = corporateEmail;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCorporateEmail() {
+        return corporateEmail;
+    }
+
+    public void setCorporateEmail(String corporateEmail) {
+        this.corporateEmail = corporateEmail;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
